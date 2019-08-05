43742ba41436a002d4b406f6d228b3350aa981fc
Ruben E Acosta <eldragondera1@gmail.com>
2019-08-05 16:16:44 -0500

    Begin loopback

---
+++ todo/src/index.ts
@@ -0,0 +1,16 @@
+import {TodoApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {TodoApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new TodoApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
